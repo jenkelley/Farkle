a653c86e6bf955be0e01902e3b6d69089fd2d44e
jenkelley <i.jen.kelley@gmail.com>
2015-03-19 13:37:50 -0500

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Jen Kelley on 3/19/15.
+//  Copyright (c) 2015 Jen Kelley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
